61d7dcb228edff82fe6e88cdcdf66dbed3543e2c
damullings17 <daniellemullings17@gmail.com>
2020-12-05 03:39:38 -0500

    Working mark as closed

--- php/displayIssue.php
+++ php/displayIssue.php
@@ -10,23 +10,39 @@
     try {
         $conn = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
         //echo "Connected to $dbname at $host successfully. <br>";
-        $query = filter_input(INPUT_GET, "query", FILTER_SANITIZE_STRING);
-        $query = strval($_GET["query"]);
+        $query = filter_input(INPUT_POST, "query", FILTER_SANITIZE_STRING);
+        $query = strval($_POST["query"]);
         
         $query = filter_var(htmlentities($query), FILTER_SANITIZE_STRING);
         $query = html_entity_decode($query,ENT_QUOTES);
-        //echo $query;
+        
         $query = intval($query);
         if(true){      
             $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            $sql = $conn->query("SELECT s.id, s.title, s.description, s.type, s.status , u.firstname, u.lastname, s.created 
+            $sql = $conn->query("SELECT s.id, s.title, s.description, s.type, s.status , u.firstname, u.lastname, s.created, s.updated 
             FROM issues s JOIN users u on s.assigned_to = u.id 
             WHERE s.id = $query ");
             
             $row = $sql->fetchAll(PDO::FETCH_ASSOC)[0];
             
         }
-        $conn = null;
+
+        if (isset($_POST["mark"]))
+        {
+            if($_POST["mark"]== "closed")
+            {
+                // This needs to be updated with the users id
+                $sql = $conn->query("UPDATE issues SET status= 'Closed'
+                WHERE issues.id = $query");
+
+            }
+            elseif($_POST["mark"]== "progress")
+            {
+                $sql = $conn->query("UPDATE issues SET status= 'In Progress'
+                WHERE issues.id = $query");
+            }    
+        }
+        
     } 
     catch (PDOException $pe) 
     {
@@ -37,8 +53,7 @@
 <h1 id="Issue title"> <?=$row['title']; ?> </h1>
 <p id="issueNo"> Issue#: <?= $row['id']?> </p>
 
-<p> <?= $row['description']?> </p>
-<p> <?=$row['created']; ?> </P>
+<p id="desc"> <?= $row['description']?> </p>
 <aside id="right">
     <div id="info">
         <h6>Assigned to:</h6>
@@ -53,7 +68,22 @@
         <h6>Status</h6>
         <p><?= $row['status']; ?></p>
     </div>
-    <button id="closed">Mark as Closed</button>
     <br>
-    <button id="progress">Mark as In Progress</button>
+            <button id="closed">Mark as Closed</button>
+            <br>
+            <button id="progress">Mark as In Progress</button>
 </aside>
+<?php
+$date = new DateTime($row['created']);
+$created = $date->format('l, F d y h:i:s');
+
+$date = new DateTime($row['updated']);
+$updated = $date->format('l, F d y h:i:s');
+?>
+<p id="grey"> 
+    
+    <img id="arrow" src="../css/little-arrow.png"> This issue was created on: <?=$created; ?> 
+    <br> 
+    <img id="arrow" src="../css/little-arrow.png"> Last updated on: <?=$updated; ?>
+</P>
+
